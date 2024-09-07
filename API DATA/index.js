fbe13a6779f41b2b7a6ab127fbb5145405c1bd01
princeessjay <vikaskashyaprock@gmail.com>
2024-09-07 17:35:51 +0530

    commit

---
+++ API DATA/index.js
@@ -0,0 +1,16 @@
+let API = "https://jsonplaceholder.typicode.com/users";
+  let nameContainer = document.getElementById("name");
+  let emailContainer = document.getElementById("email");
+
+  function collectData() {
+    fetch(API)
+      .then(response => response.json())
+      .then(data => {
+      
+        data.forEach(user => {
+          nameContainer.innerHTML += `<li>${user.name}</li>`;
+          emailContainer.innerHTML += `<li>${user.email}</li>`;
+        });
+      })
+  }
+    collectData()
--- ToDo App/app.js
+++ ToDo App/app.js
@@ -1,18 +1,50 @@
-let input = document.getElementById("InputText");
-let result = document.querySelector(".text");
+// let input = document.getElementById("InputText");
+// let result = document.querySelector(".text");
 
-function Submit() {
-    if (input.value === "") {
-        alert("Please enter your task!");
+// function Submit() {
+//     if (input.value === "") {
+//         alert("Please enter your task!");
+//     } else {
+//         let newEl = document.createElement("li");
+//         newEl.innerHTML = `${input.value} <button onclick="Delete(this)">Delete</button>`;
+//         result.appendChild(newEl);
+//         input.value = "";
+//     }
+// }
+
+// function Delete(element) {
+//     element.parentElement.remove();
+// }
+
+
+// function Submit(){
+//     if (input.value === "") {
+//         alert ("Please fill the input area!")
+//     } else {
+//         let newEl = document.createElement("li")
+//         newEl.innerHTML = `${input.value} <button onclick=Delete(this)>Delete</button>`
+//         result.appendChild(newEl)
+//         input.value = "" 
+//        }
+// }
+//    function Delete(element){
+//     element.parentElement.remove()
+//    }
+
+let inputs = document.getElementById("InputText");
+let results = document.querySelector(".text")
+
+function Submit(){
+    if (inputs.value === "") {
+        alert ("please fill the input field!")
     } else {
-        let newEl = document.createElement("li");
-        newEl.innerHTML = `${input.value} <button onclick="Delete(this)">Delete</button>`;
-        result.appendChild(newEl);
-        input.value = "";
+        let newElement = document.createElement("li")
+        newElement.innerHTML = `${inputs.value} <button onclick=Delete(this)>Delete</button>`
+        results.appendChild(newElement)
+        inputs.value = ""
     }
 }
 
-function Delete(element) {
-    element.parentElement.remove();
+function Delete(element){
+    element.parentElement.remove()
 }
-

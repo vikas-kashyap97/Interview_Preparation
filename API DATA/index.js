9d2ae1f6211fc496f78afa93aa17fd390c709ff1
princeessjay <vikaskashyaprock@gmail.com>
2024-09-08 16:31:35 +0530

    commit

--- API DATA/index.js
+++ API DATA/index.js
@@ -1,16 +1,38 @@
-let API = "https://jsonplaceholder.typicode.com/users";
-  let nameContainer = document.getElementById("name");
-  let emailContainer = document.getElementById("email");
+// let API = "https://jsonplaceholder.typicode.com/users";
+//   let nameContainer = document.getElementById("name");
+//   let emailContainer = document.getElementById("email");
 
-  function collectData() {
-    fetch(API)
-      .then(response => response.json())
-      .then(data => {
+//   function collectData() {
+//     fetch(API)
+//       .then(response => response.json())
+//       .then(data => {
       
-        data.forEach(user => {
-          nameContainer.innerHTML += `<li>${user.name}</li>`;
-          emailContainer.innerHTML += `<li>${user.email}</li>`;
-        });
-      })
-  }
-    collectData()
+//         data.forEach(user => {
+//           nameContainer.innerHTML += `<li>${user.name}</li>`;
+//           emailContainer.innerHTML += `<li>${user.email}</li>`;
+//         });
+//       })
+//   }
+//     collectData()
+
+
+// let API = "https://jsonplaceholder.typicode.com/users"
+// let nameContainer = document.getElementById("name")
+// let emailContainer = document.getElementById("email")
+
+// function CollectData(){
+//   fetch(API)
+//   .then(response => response.json())
+//   .then (data => {
+//     data.forEach(user => {
+//         nameContainer.innerHTML += `<li>${user.name}</li>`
+//         emailContainer.innerHTML += `<li>${user.email}</li>`
+//     });
+//   })
+// }
+
+// CollectData()
+
+
+
+
--- ToDo App/app.js
+++ ToDo App/app.js
@@ -31,27 +31,38 @@
 //     element.parentElement.remove()
 //    }
 
+// let inputs = document.getElementById("InputText");
+// let results = document.querySelector(".text")
+
+// function Submit() {
+//     if (inputs.value === "") {
+//         alert ("Please fill the input field!")
+//     } else {
+//         let newEl = document.createElement("li")
+//         newEl.innerHTML = `${inputs.value}  <button onclick=Delete(this)>X</button>`
+//         results.appendChild(newEl)
+//         inputs.value = ""
+//     }
+// }
+//     function Delete(element){
+//         element.parentElement.remove()
+//     }
+
+
 let inputs = document.getElementById("InputText");
 let results = document.querySelector(".text")
 
 function Submit(){
     if (inputs.value === "") {
-        alert ("please fill the input field!")
+        alert ("Please fill the input field!")
     } else {
         let newElement = document.createElement("li")
-        newElement.innerHTML = `${inputs.value} <button onclick=Delete(this)>Delete</button>`
+        newElement.innerHTML = `${inputs.value}  <button onclick=Delete(this)>Delete</button>`
         results.appendChild(newElement)
-        inputs.value = ""
+        inputs.value= ""
     }
 }
-<<<<<<< HEAD
 
-function Delete(element){
-    element.parentElement.remove()
+function Delete(Element){
+     Element.parentElement.remove()
 }
-=======
-function Delete(element) {
-    element.parentElement.remove();
-}
-
->>>>>>> 1d881106e690b1fd02541775cfa92e509aff63ef
